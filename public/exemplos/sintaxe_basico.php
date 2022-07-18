deacd95c0f496f41d1589ba47eb5db544eef7a97
Luan Cardoso <luanccardoso23@gmail.com>
2022-07-18 20:58:37 -0300

    Inicializado projeto evento com php

---
+++ public/exemplos/sintaxe_basico.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sintaxe Básica PHP</title>
+</head>
+<body>
+    <h1>Exemplo de texto</h1>
+    <?php 
+    
+    echo "<h1> Oi, tudo bem?</h1>";
+    $variavel = 10;
+    echo "\n<p>$variavel</p>";
+    
+    ?>
+
+</body>
+</html>
