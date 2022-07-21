d0ec50ab97d6a99c2dc25ddeea9924444bd8c2c5
Luan Cardoso <luanccardoso23@gmail.com>
2022-07-21 20:05:18 -0300

    Finalizado função de validação de data

--- app/evento.php
+++ app/evento.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set("America/Sao_Paulo");
 
 $nomeEvento = $_POST["nomeEvento"];
 $dataEvento = $_POST["dataEvento"];
@@ -13,8 +14,20 @@ var_dump($_POST);
 function validaData($data){
 
     $dataEvento = new DateTime($data);
+    $dataAtual = new DateTime("now");
 
     echo $dataEvento->format("d/m/Y");
+    echo "<br> A data de hoje é: ";
+    print_r($dataAtual);
+
+
+    if($dataEvento >= $dataAtual){
+        echo "<p>Data do evento cadastrado com sucesso!</p>";
+
+    }
+    else{
+        echo "<p>A data do evento não pode ser anterior a data atual!</p>";
+    }
 
 }
 validaData($dataEvento);
