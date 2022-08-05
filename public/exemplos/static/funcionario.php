815584abcd0b021e752b0d977dfdf3d296e02f08
Luan Cardoso <luanccardoso23@gmail.com>
2022-08-05 19:14:22 -0300

    Finalizado estudo do static

--- public/exemplos/static/funcionario.php
+++ public/exemplos/static/funcionario.php
@@ -5,10 +5,16 @@ class Funcionario{
     private static $nome;
     private static $salario;
 
-    public function relatorio($nome, $salario){
+    public static function relatorio($nome, $salario){
         self::$nome = $nome;
         self::$salario = $salario;
 
-        echo "Olá ".self::$nome" seu salário é ".self::$salario." reais";
+        echo "Olá ".self::$nome." seu salário é ".self::$salario." reais";
+    }
+    public static function getSalario(){
+       return self::$salario; 
+    }
+    public static function setSalario($valor){
+     self::$salario = $valor;
     }
 }
--- public/exemplos/static/index.php
+++ public/exemplos/static/index.php
@@ -9,3 +9,5 @@ require("funcionario.php");
 //$f1->relatorio("Zé Gotinha", 200);
 
 Funcionario::relatorio("Ana", 1500);
+Funcionario::setSalario(3000);
+echo "<br>" .Funcionario::getSalario();
