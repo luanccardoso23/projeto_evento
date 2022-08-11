a6fb647de44f7a21722410ab83437a14798d7efd
Luan Cardoso <luanccardoso23@gmail.com>
2022-08-11 20:56:28 -0300

    finalizado

--- app/classes/Evento.php
+++ app/classes/Evento.php
@@ -2,10 +2,12 @@
 
 class Evento{
    public $nomeEvento;
-  public  $dataEvento;
+   public $dataEvento;
+   public $banner;
+   public $mensagem = [];// armazenar as mensagens de erro e sucesso
 
   // METODOS
-  function validaData($data){
+ public function validaData($data){
 
     $dataEvento = new DateTime($data);
     $dataAtual = new DateTime("now");
@@ -16,11 +18,13 @@ class Evento{
 
 
     if($dataEvento >= $dataAtual){
-        echo "<p>Data do evento cadastrado com sucesso!</p>";
+        //echo "<p>Data do evento cadastrado com sucesso!</p>";
+        return true;
 
     }
     else{
-        echo "<p>A data do evento não pode ser anterior a data atual!</p>";
+        //echo "<p>A data do evento não pode ser anterior a data atual!</p>";
+        return false;
     }
 
 
@@ -28,11 +32,62 @@ class Evento{
 
 }
 
-function recebeDados($campos){
+public function recebeDados($campos){
     
     $this->nomeEvento = $campos["nomeEvento"];
     $this->dataEvento = $campos["dataEvento"];
-    echo "O {$this->nomeEvento} vai acontecer na data {$this->dataEvento}";
+    if(empty($this->nomeEvento) || empty($this->dataEvento)){
+        return 
+
+    }
+    else{
+        return true;
+    }
+    
+    
+    
+    //echo "O {$this->nomeEvento} vai acontecer na data {$this->dataEvento}";
+
+}
+// essa função irá receber o comando no padrão $_FILES['nome_arquivo']
+public function recebeArquivo($banner){
+    
+ //$nomeArquivo = $_FILES["banner"]["name"];
+ //$nomeTemporario = $_FILES["banner"]["tmp_name"];
+ $this->banner = $banner;// estou atribuindo $_FILES['banner'] para $this->banner
+ 
+
+
+
+ if(empty($this->banner["name"])){
+    echo "<br> Arquivo Vazio";
+ }
+ else{
+     echo "<br> Arquivo Aceito";
+     $infoArquivo = pathinfo($this->banner["name"]);
+     echo "<br>";
+     print_r($infoArquivo);
+     if($infoArquivo["extension"] == "jpg" || $infoArquivo["extension"] == "png"){
+         echo "<br> formato de arquivo aceito";
+         $pasta = "../imagens/";
+        //iremos verificar se a pasta existe ou não
+        if(!file_exists($pasta)){
+            mkdir($pasta,0777,true);// a função mkdir precisa de 3 parâmetros: 1. nome da pasta, 2. permissão para ler e escrever na pasta, 3. se irar criar subpastas ou não.
+
+        }
+
+
+         $caminhoFinal = $pasta.$this->banner["name"];
+         move_uploaded_file($this->banner["tmp_name"], $caminhoFinal);
+         echo "<img src='{$caminhoFinal}' width='200px' height='200px'>";
+
+     }
+     else{
+        echo "<br> formato de arquivo inválido";
+
+     }
+
+ }
 
 }
 
--- app/recebe_imagem.php
+++ app/recebe_imagem.php
@@ -24,8 +24,16 @@ echo "<pre>";
      if($infoArquivo["extension"] == "jpg" || $infoArquivo["extension"] == "png"){
          echo "<br> formato de arquivo aceito";
          $pasta = "imagens/";
+        //iremos verificar se a pasta existe ou não
+        if(!file_exists($pasta)){
+            mkdir($pasta,0777,true);// a função mkdir precisa de 3 parâmetros: 1. nome da pasta, 2. permissão para ler e escrever na pasta, 3. se irar criar subpastas ou não.
+
+        }
+
+
          $caminhoFinal = $pasta.$nomeArquivo;
          move_uploaded_file($nomeTemporario, $caminhoFinal);
+         echo "<img src='{$caminhoFinal}' width='200px' height='200px'>";
 
      }
      else{
